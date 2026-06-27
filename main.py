56ac324b3d883c2fc84aad7e82c8756ccc6f78f9
larendelrey12 <meggettolara2@gmail.com>
2026-06-27 16:46:57 +0200

    Refactor birthday email notification logic
    Refactor birthday email script to improve readability and functionality.

--- main.py
+++ main.py
@@ -1,38 +1,49 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
+import datetime as dt
 import pandas
-import random
 import smtplib
-import os
+from email.mime.text import MIMEText
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+today = dt.datetime.now()
+m = today.month
+d = today.day
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+with open("birthdays.csv", encoding="utf-8") as f:
+    data = pandas.read_csv(f)
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+birthday_people = data.query('month == @m and day == @d')
+try:
+    new_names = [birthday_people["name"].item()]
+except ValueError:
+    new_names = [n[1]["name"] for n in birthday_people.iterrows()]
+print(birthday_people)
+print(new_names)
+
+###
+
+with open("Letter.txt", "r", encoding="utf-8") as f:
+    letter = f.read()
+    letters = []
+    for name in new_names:
+        new = letter.replace("[NAME]", name)
+        letters.append(new)
+    dic_letters = dict(zip(new_names, letters))
+    print(dic_letters)
+
+
+
+
+
+# 4. Send the letter generated in step 3 to that person's email address.
+my_email = "EMAIL"
+psw = "PASSWORD"
+with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+    connection.starttls() # this encrypts the email in case it is intercepted
+    connection.login(my_email, psw)
+    for n in dic_letters:
+        email = birthday_people.query("name == @n")["email"].item()
+        msg = MIMEText(f"{dic_letters[n]}", "plain", "utf-8")
+        msg["Subject"] = "Birthday"
+        msg["From"] = my_email
+        msg["To"] = email
+        connection.sendmail(msg["From"], msg["To"], msg.as_string())
